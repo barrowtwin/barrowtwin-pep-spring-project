c6de810f9a0da50fc5ccd05d624faf5895d55f9e
Eric Barrow <barrowtwin@gmail.com>
2025-06-19 02:22:49 +0000

    Created repositories and started implementation of services.

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,24 @@
 package com.example.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.repository.AccountRepository;
+import com.example.entity.Account;
+
+@Service
 public class AccountService {
+
+    private AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public List<Account> getAccountList() {
+        return (List<Account>) accountRepository.findAll();
+    }
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,4 +1,26 @@
 package com.example.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.repository.MessageRepository;
+import com.example.entity.Account;
+import com.example.entity.Message;
+
+@Service
 public class MessageService {
+
+    private MessageRepository messageRepository;
+
+    @Autowired
+    public MessageService(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Message> getMessageList() {
+        return (List<Message>) messageRepository.findAll();
+    }
 }
